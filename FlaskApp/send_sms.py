e1210a539732248494587c5508d49ca714e51fa4
jigar <jigshahpuzzle@gmail.com>
2016-04-16 17:48:31 -0500

    fixed

--- FlaskApp/send_sms.py
+++ FlaskApp/send_sms.py
@@ -6,13 +6,10 @@ def sendMessage(confidence_score, phone_number):
 	client = TwilioRestClient(account_sid, auth_token)
 	if confidence_score > 0.9:
 		message = client.messages.create(to=phone_number, from_="+14245439007",
-	                                     body="Hello there! Our algorithms say that you have a %d chance of having skin cancer." + 
-	                                     "We highly recommend you seek medical attention immediately." % (confidence_score))
+	                                     body="Hello there! Our algorithms say that you have a %d chance of having skin cancer. We highly recommend you seek medical attention immediately." % (confidence_score))
 	elif confidence_score > 0.5:
 		message = client.messages.create(to=phone_number, from_="+14245439007",
-	                                     body="Hello there! Our algorithms say that you have a %d chance of having skin cancer." + 
-	                                     "It would be advisable to get it checked out!" % (confidence_score))
+	                                     body="Hello there! Our algorithms say that you have a %d chance of having skin cancer. It would be advisable to get it checked out!" % (confidence_score))
 	else: 		
 		message = client.messages.create(to=phone_number, from_="+14245439007",
-	                                     body="Hello there! Our algorithms say that you have a %d chance of having skin cancer." + 
-	                                     "You are probably okay!" % (confidence_score))
+	                                     body="Hello there! Our algorithms say that you have a %d chance of having skin cancer. You are probably okay!" % (confidence_score))
