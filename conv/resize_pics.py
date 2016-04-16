b3c0cf22bdfbc98f23706fd558e65aca8a6965b6
jigar <jigshahpuzzle@gmail.com>
2016-04-16 17:28:52 -0500

    updated

--- conv/resize_pics.py
+++ conv/resize_pics.py
@@ -6,6 +6,6 @@ def resizeToSquare(path):
 	imgW = float(img.size[0])
 	imgH = float(img.size[1])
 	imgRatio = imgH / imgW
-	if imgRatio > 0.7 or imgRatio < 1.3
-	img = img.resize((500,500), PIL.Image.ANTIALIAS)
-	img.save('output' + path)
+	if imgRatio > 0.7 or imgRatio < 1.3:
+		img = img.resize((500,500), PIL.Image.ANTIALIAS)
+		img.save('output' + path)
