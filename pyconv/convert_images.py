9f1a27fe5a99ee8dac01fd3439ab46f2673f5635
jigar <jigshahpuzzle@gmail.com>
2016-04-17 11:27:32 -0500

    complete

--- pyconv/convert_images.py
+++ pyconv/convert_images.py
@@ -1,6 +1,7 @@
 import cPickle
 from PIL import Image
 import numpy
+import theano
 
 
 if __name__ == "__main__":
@@ -52,35 +53,59 @@ if __name__ == "__main__":
 	f.close()
 
 
-	# my_x = []
-	# my_y = []
-	# with open('path_to_file', 'r') as f:
-	#     for line in f:
-	#         my_list = line.split(' ') # replace with your own separator instead
-	#         my_x.append(my_list[1:-1]) # omitting identifier in [0] and target in [-1]
-	#         my_y.append(my_list[-1])
-	# valid_set_x = theano.shared(numpy.array(my_x, dtype='float64'))
-	# valid_set_y = theano.shared(numpy.array(my_y, dtype='float64'))
+	my_x = []
+	my_y = []
+	for item in validate: 
+		if item:
+			im = Image.open("skin_data/" + item).convert('L')
+			pixels = list(im.getdata())
+			width, height = im.size
+			pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
+			pixels2 = []
+			for lst in pixels:
+				for num in lst:
+					pixels2.append(num)
 
-	# f = file('valid_set.pkl', 'wb')
-	#     cPickle.dump((valid_set_x, valid_set_y), f, protocol=cPickle.HIGHEST_PROTOCOL)
-	# f.close()
+			my_x.append(pixels2)
+			if "mmdermis" in item or "mmdermquest" in item: 
+				my_y.append([0])
+			else: 
+				my_y.append([1])
+
+	valid_set_x = theano.shared(numpy.array(my_x, dtype='float64'))
+	valid_set_y = theano.shared(numpy.array(my_y, dtype='float64'))
+
+	f = file('valid_set.pkl', 'wb')
+	    cPickle.dump((valid_set_x, valid_set_y), f, protocol=cPickle.HIGHEST_PROTOCOL)
+	f.close()
 
 
-	# my_x = []
-	# my_y = []
-	# with open('path_to_file', 'r') as f:
-	#     for line in f:
-	#         my_list = line.split(' ') # replace with your own separator instead
-	#         my_x.append(my_list[1:-1]) # omitting identifier in [0] and target in [-1]
-	#         my_y.append(my_list[-1])
-	# test_set_x = theano.shared(numpy.array(my_x, dtype='float64'))
-	# test_set_y = theano.shared(numpy.array(my_y, dtype='float64'))
+	my_x = []
+	my_y = []
+	for item in test: 
+		if item:
+			im = Image.open("skin_data/" + item).convert('L')
+			pixels = list(im.getdata())
+			width, height = im.size
+			pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
+			pixels2 = []
+			for lst in pixels:
+				for num in lst:
+					pixels2.append(num)
+
+			my_x.append(pixels2)
+			if "mmdermis" in item or "mmdermquest" in item: 
+				my_y.append([0])
+			else: 
+				my_y.append([1])
+	
+	test_set_x = theano.shared(numpy.array(my_x, dtype='float64'))
+	test_set_y = theano.shared(numpy.array(my_y, dtype='float64'))
 
 
-	# f = file('test_set.pkl', 'wb')
-	#     cPickle.dump((test_set_x, test_set_y), f, protocol=cPickle.HIGHEST_PROTOCOL)
-	# f.close()
+	f = file('test_set.pkl', 'wb')
+	    cPickle.dump((test_set_x, test_set_y), f, protocol=cPickle.HIGHEST_PROTOCOL)
+	f.close()
 
 
 
