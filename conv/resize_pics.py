c5a6d90fb5fe4e4821a2bf9f0ca6d9203cc82e2a
jigar <jigshahpuzzle@gmail.com>
2016-04-16 20:07:12 -0500

    resize pics added

--- conv/resize_pics.py
+++ conv/resize_pics.py
@@ -7,6 +7,32 @@ def resizeToSquare(path):
 	imgW = float(img.size[0])
 	imgH = float(img.size[1])
 	imgRatio = imgH / imgW
-	if imgRatio > 0.7 or imgRatio < 1.3:
-		img = img.resize((500,500), PIL.Image.ANTIALIAS)
+	if imgW == imgH:
+		img = img.resize((512,512), PIL.Image.ANTIALIAS)
 		img.save('output' + path)
+	elif imgW > imgH: 
+		widthRatio = 512 / float(imgW)
+		img = img.resize(widthRatio * imgW, widthRatio * imgH), PIL.Image.ANTIALIAS)
+		img.save('output' + path)
+		img_size = img.size
+		new_size = (512, 512)
+		new_im = Image.new("RGB", new_size)   ## luckily, this is already black!
+		new_im.paste(img, ((new_size[0]-img_size[0])/2,
+		                      (new_size[1]-img_size[1])/2))
+		new_im.save('output' + path)
+	elif imgW < imgH: 
+		heighRatio = 512 / float(imgH)
+		img = img.resize(heighRatio * imgW, heighRatio * imgH), PIL.Image.ANTIALIAS)
+		img_size = img.size
+
+		new_size = (512, 512)
+		new_im = Image.new("RGB", new_size)   ## luckily, this is already black!
+		new_im.paste(img, ((new_size[0]-img_size[0])/2,
+		                      (new_size[1]-img_size[1])/2))
+
+
+		new_im.save('output' + path)
+
+
+
+
